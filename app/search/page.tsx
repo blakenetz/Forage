92810b5ac3cd99e160ef7eefaee899e5bca8287e
Blake Netzeband <blake.netzeband@gmail.com>
2024-10-05 14:51:51 -0600

    refactor to grid

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,13 +1,25 @@
 import NextImage from "next/image";
 
 import { param } from "@/util";
-import { Card, CardSection, Text, Title, Anchor } from "@mantine/core";
+import {
+  Card,
+  CardSection,
+  Text,
+  Title,
+  Anchor,
+  SimpleGrid,
+  Accordion,
+  AccordionItem,
+  AccordionPanel,
+  AccordionControl,
+} from "@mantine/core";
 import { redirect } from "next/navigation";
 
 import { type NextRequest } from "next/server";
 import { queryNyTimes } from "./actions";
 import styles from "./search.module.css";
 import Meta from "./meta";
+import { IconBrandNytimes } from "@tabler/icons-react";
 
 export function GET(request: NextRequest) {
   const { searchParams } = request.nextUrl;
@@ -27,40 +39,47 @@ export default async function Search({
   return (
     <main className={styles.main}>
       <Title>Recipeas!</Title>
-      <section className={styles.results}>
-        <section className={styles.section}>
-          {data.map((d) => (
-            <Anchor
-              key={d.title}
-              href={d.link}
-              rel="noopener noreferrer"
-              target="__blank"
-            >
-              <Card
-                shadow="sm"
-                padding="xs"
-                radius="md"
-                withBorder
-                className={styles.card}
-              >
-                <CardSection className={styles.imageWrapper}>
-                  <NextImage
-                    src={d.img}
-                    alt={d.title}
-                    sizes="300px"
-                    fill
-                    className={styles.image}
-                  />
-                </CardSection>
+      <Accordion variant="filled">
+        <AccordionItem value="nyTimes">
+          <AccordionControl icon={<IconBrandNytimes />}>
+            New York Times Cooking
+          </AccordionControl>
+          <AccordionPanel>
+            <SimpleGrid cols={5} spacing="xs">
+              {data.map((d) => (
+                <Anchor
+                  key={d.title}
+                  href={d.link}
+                  rel="noopener noreferrer"
+                  target="__blank"
+                >
+                  <Card
+                    shadow="sm"
+                    padding="xs"
+                    radius="md"
+                    withBorder
+                    className={styles.card}
+                  >
+                    <CardSection className={styles.imageWrapper}>
+                      <NextImage
+                        src={d.img}
+                        alt={d.title}
+                        sizes="300px"
+                        fill
+                        className={styles.image}
+                      />
+                    </CardSection>
 
-                <Title order={3}>{d.title}</Title>
-                {d.description && <Text>{d.description}</Text>}
-                <Meta {...d.meta} />
-              </Card>
-            </Anchor>
-          ))}
-        </section>
-      </section>
+                    <Title order={3}>{d.title}</Title>
+                    {d.description && <Text>{d.description}</Text>}
+                    <Meta {...d.meta} />
+                  </Card>
+                </Anchor>
+              ))}
+            </SimpleGrid>
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
     </main>
   );
 }
