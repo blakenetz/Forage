d164a3bcc8b50d99ab662658c44f6323671ad219
Blake Netzeband <blake.netzeband@gmail.com>
2024-11-11 15:35:15 -0700

    Added react transition lib

--- app/search/components/searchControl.tsx
+++ app/search/components/searchControl.tsx
@@ -1,30 +1,51 @@
-import { ActionIcon, InlineStyles, TextInput } from "@mantine/core";
+"use client";
+import { ActionIcon, TextInput } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { IconSearch } from "@tabler/icons-react";
 import styles from "../search.module.css";
-import { CSSVariable } from "@/types";
+import { Transition, TransitionStatus } from "react-transition-group";
+import { CSSProperties, useRef } from "react";
+import clsx from "clsx";
+
+const defaultStyle: CSSProperties = {
+  transform: "translateX(100%)",
+  transition: "transform 500ms",
+};
+
+const transitionStyles: Partial<Record<TransitionStatus, CSSProperties>> = {
+  entering: { transform: "translateX(0)" },
+  entered: { transform: "translateX(0)" },
+  exiting: defaultStyle,
+  exited: defaultStyle,
+};
 
 export default function SearchControl() {
   const [open, { toggle }] = useDisclosure(false);
-
-  const inlineStyles: CSSVariable = {
-    "--search-translate": open ? "0" : "100%",
-  };
+  const ref = useRef<HTMLInputElement>(null);
 
   return (
-    <>
-      <InlineStyles selector={`.${styles.searchInput}`} styles={inlineStyles} />
-      <TextInput
-        classNames={{
-          input: styles.searchInput,
-          wrapper: styles.searchWrapper,
-        }}
-        rightSection={
-          <ActionIcon variant="subtle" onClick={toggle}>
-            <IconSearch />
-          </ActionIcon>
-        }
-      />
-    </>
+    <Transition in={open} timeout={500}>
+      {(state) => {
+        const isActive = state === "entering" || state === "exiting";
+
+        return (
+          <TextInput
+            classNames={{
+              wrapper: clsx(
+                styles.searchWrapper,
+                isActive && styles.searchWrapperActive
+              ),
+            }}
+            ref={ref}
+            styles={{ input: { ...defaultStyle, ...transitionStyles[state] } }}
+            rightSection={
+              <ActionIcon variant="subtle" onClick={toggle}>
+                <IconSearch />
+              </ActionIcon>
+            }
+          />
+        );
+      }}
+    </Transition>
   );
 }
