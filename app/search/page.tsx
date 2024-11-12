3a20e04323ef38d0d218a426f4503851183c26c2
Blake Netzeband <blake.netzeband@gmail.com>
2024-11-11 18:52:54 -0700

    fix build errors

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,17 +1,8 @@
 import { param } from "@/util";
 import { redirect } from "next/navigation";
 
-import { type NextRequest } from "next/server";
-
 import { fetchRecipeData } from "./actions";
 import View from "./components/view";
-
-export function GET(request: NextRequest) {
-  const { searchParams } = request.nextUrl;
-  const query = searchParams.get(param);
-  if (!query) redirect("/");
-  return query;
-}
 
 export default async function Search({
   searchParams,
@@ -19,6 +10,8 @@ export default async function Search({
   searchParams: { [param]: string };
 }) {
   const query = searchParams[param];
+  if (!query) redirect("/");
+
   const data = await fetchRecipeData(query);
 
   return <View data={data} />;
